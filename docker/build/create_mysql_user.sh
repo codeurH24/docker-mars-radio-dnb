2c3c68ca2a97624538ca83d373a2e17929208933
codeurh24 <cci.corlouer@gmail.com>
2020-04-21 00:22:30 +0200

    git tracking files dans le dossier docker/build

---
+++ docker/build/create_mysql_user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -e /stack/create_mysql_user.log]
+then
+    echo "Fichier create_mysql_user.log existe" >> /stack.log
+else
+    touch /stack/create_mysql_user.log
+    mysql -u root -e "CREATE USER 'codeurh24'@'%' IDENTIFIED BY 'password';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'codeurh24'@'%';"
+    echo "User mysql created" >> /stack.log
+fi
